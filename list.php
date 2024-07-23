efad490f1d0d12d5ae05256aa1a95b7c42fca98f
Hanis siddiq <hanissiddiq10@gmail.com>
2024-07-23 08:57:09 +0700

    Modif templat tampilan dengan Materialize

--- list.php
+++ list.php
@@ -25,7 +25,7 @@ $result = mysqli_fetch_all($query, MYSQLI_ASSOC);
       name="viewport"
       content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-    <title>Blank layout - Layouts | Materialize - Material Design HTML Admin Template</title>
+    <title>Daftar Pegawai</title>
 
     <meta name="description" content="" />
 
